675b28f2f9cf6d76b617b49a21c2eb323ed7eb46
Anne LoVerso <anne.loverso@gmail.com>
2022-04-14 00:49:44 -0400

    update document title

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,6 +3,7 @@ import React, { ReactElement } from "react";
 import { AppProps } from "next/app";
 
 const App = ({ Component, pageProps }: AppProps): ReactElement => {
+  document.title = "Justus RPG Dice Roller"
   return (
     <div>
       <Component {...pageProps} />
